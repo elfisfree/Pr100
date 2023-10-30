23c35e70d1b7e642922bd0f3f435f7b30d5942de
Вероника <148047290+elfisfree@users.noreply.github.com>
2023-10-30 19:32:29 +0300

    Изменение в названии моделей

--- Pr100/Classes/Transport.cs
+++ Pr100/Classes/Transport.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Pr100.Classes
+﻿namespace Pr100.Classes
 {
     abstract class Transport
     {
